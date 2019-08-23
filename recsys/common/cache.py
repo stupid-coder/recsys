3b54b611db983f8a058867a085faa17e76dd2ea7
stupid-coder <qixiang@staff.sina.com.cn>
2019-08-23 15:27:07 +0800

    增加cache输入日志

--- recsys/common/cache.py
+++ recsys/common/cache.py
@@ -11,12 +11,15 @@ def cached(cache_path):
     def cache_wrapper(func):
         def _do(*argc, **argv):
             if cache_path in _do.__dict__:
+                print("{} cached return")
                 return _do.__dict__[cache_path]
             else:
                 if os.path.exists(cache_path):
+                    print("{} cached load".format(cache_path))
                     result = np.load(open(cache_path, "rb"), allow_pickle=True)
                     _do.__dict__[cache_path] = result
                 else:
+                    print("{} cached run")
                     _do.__dict__[cache_path] = func(*argc, **argv)
                     if isinstance(_do.__dict__[cache_path], (ma.MaskedArray, np.ndarray)):
                         _do.__dict__[cache_path].dump(cache_path)
